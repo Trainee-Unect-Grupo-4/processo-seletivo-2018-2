165c570d8c9c1c18b191b1765a92306479e12c1c
gustavooresende <gustavoo3108@gmail.com>
2018-10-03 22:43:50 -0300

    database

--- routes/api/local.js
+++ routes/api/local.js
@@ -33,30 +33,30 @@ router.get("/", (req, res) => {
 });
 
 //Exibindo locais do banco seguindo um filtro
-router.post("/filtro", (req, res) => {
-  //console.log(req.body.moradia);
-  //var filtro = JSON.parse(req.body.moradia);
-  var consulta = Local.find();
-  if (req.body.moradia != undefined) {
-    consulta = consulta.find({ moradia: req.body.moradia });
-  }
-  if (req.body.vaga != undefined) {
-    consulta = consulta.find({ vagas: req.body.vaga });
-  }
-  if (req.body.valor != undefined) {
-    consulta = consulta.find({ valor: req.body.valor });
-  }
-  if (req.body.quarto != undefined) {
-    consulta = consulta.find({ quarto: req.body.quarto });
-  }
-  if (req.body.mobilia != undefined) {
-    consulta = consulta.find({ mobilia: req.body.mobilia });
-  }
-  if (req.body.grupo != undefined) {
-    consulta = consulta.find({ grupo: req.body.grupo });
-  }
-  const local = consulta;
-  local.then(local => res.json({ array: local }));
-});
+// router.post("/filtro", (req, res) => {
+//   //console.log(req.body.moradia);
+//   //var filtro = JSON.parse(req.body.moradia);
+//   var consulta = Local.find();
+//   if (req.body.moradia != undefined) {
+//     consulta = consulta.find({ moradia: req.body.moradia });
+//   }
+//   if (req.body.vaga != undefined) {
+//     consulta = consulta.find({ vagas: req.body.vaga });
+//   }
+//   if (req.body.valor != undefined) {
+//     consulta = consulta.find({ valor: req.body.valor });
+//   }
+//   if (req.body.quarto != undefined) {
+//     consulta = consulta.find({ quarto: req.body.quarto });
+//   }
+//   if (req.body.mobilia != undefined) {
+//     consulta = consulta.find({ mobilia: req.body.mobilia });
+//   }
+//   if (req.body.grupo != undefined) {
+//     consulta = consulta.find({ grupo: req.body.grupo });
+//   }
+//   const local = consulta;
+//   local.then(local => res.json({ array: local }));
+// });
 
 module.exports = router;
